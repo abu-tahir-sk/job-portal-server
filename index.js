d44ba03f5fea65786816f08e0a32f7054ef9e6eb
abu-tahir-sk <abutahir2626@gmail.com>
2025-11-12 20:13:11 -1000

    jobs data

---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qha6rup.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected");
+
+    // jobs related apis
+    //jobs collection
+    const jobsCollection = client.db("jobsPortal").collection("jobs");
+    // jobs application collection
+    const jobApplicationCollection = client
+      .db("jobsPortal")
+      .collection("job_applications");
+
+    // jobs
+    app.get("/jobs", async (req, res) => {
+      const cursor = jobsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // 1 job
+
+    app.get("/jobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobsCollection.findOne(query);
+      res.send(result);
+    });
+
+    // job application apis
+    app.post("/job-applications", async (req, res) => {
+      const application = req.body;
+      const result = await jobApplicationCollection.insertOne(application);
+      res.send(result);
+    });
+  } finally {
+    //     await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("job is falling from the sky");
+});
+
+app.listen(port, () => {
+  console.log(`Job is waiting at: ${port}`);
+});
